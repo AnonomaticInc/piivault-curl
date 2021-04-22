09e75af0bcdd593deec1f0af0751175ed3b1adc6
lindsay.lawrence <lindsay@anonomatic.com>
2021-04-22 10:04:07 -0700

    for api example use

--- curl-commands.sh
+++ curl-commands.sh
@@ -15,50 +15,216 @@ __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on
 
 #arg1="${1:-}"
 
+# Lindsay
+#
+# SubscriptionId: 4e5d7305-0cc6-6d6b-6453-cc16be164e68
+#
+#
+# Web Access https://piivault.web
+# Email: llawrence.dev@gmail.com
+# Password: HNYAHS6E
+#
+# API Access https://piivault.web.api
+# AccountId: 2cb1211b-214a-728b-a40f-1939503ba153
+# ApiKey: KGVgpjdXWtSmD0SzkeZio6rFXcWHmwZn
+#
+# Lindsay
+#
+# SubscriptionId: 4e5d7305-0cc6-6d6b-6453-cc16be164e68
+#
+#
+# Web Access
+# Email: lindsay@anonomatic.com
+# Password: OPBN45JR
+#
+# API Access
+# AccountId: 88fb13a4-b14d-b1fb-cac5-9da45ad29a51
+# ApiKey: 3a98wl4UBxWpDTbDPF8DCLkaKe6Vfosb
+
+HOSTNAME=localhost:62291
+#JSON2SEND=sunder-test-10k.json
+
+ACCOUNTID_1=88fb13a4-b14d-b1fb-cac5-9da45ad29a51
+APIKEY_1=3a98wl4UBxWpDTbDPF8DCLkaKe6Vfosb
+
+ACCOUNTID_2=2cb1211b-214a-728b-a40f-1939503ba153
+APIKEY_2=KGVgpjdXWtSmD0SzkeZio6rFXcWHmwZn
+
+# ./curl-commands.sh --HOSTNAME "localhost:53043" --VERB PurgeAccountProfile --ACCOUNTID 88fb13a4-b14d-b1fb-cac5-9da45ad29a51 --APIKEY 3a98wl4UBxWpDTbDPF8DCLkaKe6Vfosb
+# ./curl-commands.sh --HOSTNAME "localhost:53043" --VERB GetPolyIdBulk --ACCOUNTID 88fb13a4-b14d-b1fb-cac5-9da45ad29a51 --APIKEY 3a98wl4UBxWpDTbDPF8DCLkaKe6Vfosb --REQUEST sunder-test-10k.json
+# ./curl-commands.sh --HOSTNAME "localhost:53043" --VERB Match --ACCOUNTID 88fb13a4-b14d-b1fb-cac5-9da45ad29a51 --APIKEY 3a98wl4UBxWpDTbDPF8DCLkaKe6Vfosb --REQUEST match-request.json
+# ./curl-commands.sh --HOSTNAME "localhost:53043" --VERB GetProfilePseudonym --ACCOUNTID 88fb13a4-b14d-b1fb-cac5-9da45ad29a51 --APIKEY 3a98wl4UBxWpDTbDPF8DCLkaKe6Vfosb --REQUEST getprofilepseudonym-request.json
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+        echo $1 $2
+   fi
+
+  shift
+done
+
+case $VERB in
+
+login)
 # 1. Use the AccountId, and API Key provided by Anonomatic to get a Bearer Token
-
 # -- POST login --
-#curl \
-#-X POST "https://api.piivault.com/api/auth/login" \
-#-H "Content-Type: application/json" \
-#-d '{ "AccountId": "[ Account ID ]", "ApiKey": "[ API Key ]" }' \
-#> api-token.txt
+rm api-token.txt
 
-# 2. Copy the token from the JSON Payload of the above response to here
+curl -k \
+	-X POST "https://${HOSTNAME}/api/auth/login" \
+	-H "Content-Type: application/json" \
+-d "{ \"AccountId\": \"${ACCOUNTID}\", \"ApiKey\": \"${APIKEY}\" }" \
+> api-token.txt
+exit
 
-API_TOKEN=YourApiTokenHere
+;;
 
-# 3. Try out the apis below! 
-# NOTE: Uncomment each curl command in turn
+esac
 
+# 2. Copy the token from the JSON Payload of the above response to here (or use jq)
+# NOTE: This uses https://stedolan.github.io/jq/ to set the API_TOKEN
+
+API_TOKEN=$(jq -r ".Data.Token" api-token.txt)
+
+
+# 3. Try out the apis below!
+case $VERB in
+
+GetPolyId)
 ## -- PUT GetPolyId --
 
+echo
+echo "START //${HOSTNAME}/api/profiles/GetPolyId: $(date)"
+echo
+
 curl -v \
- -X PUT "https://api.piivault.com/api/profiles/GetPolyId" \
+ -X PUT "https://${HOSTNAME}/api/profiles/GetPolyId" \
  -H "Authorization: Bearer ${API_TOKEN}" \
  -H "Content-Type: application/json" \
- -H "Content-Type: application/json" -d @test-profile-min.json
+ -H "Content-Type: application/json" -d @${REQUEST} > getpolyid-response.json
 
+echo
+echo "FINIS GetPolyId: $(date)"
+echo
+
+;;
+
+GetPolyIdBulk)
 ## -- PUT PolyIdBulk --
 
-#curl -v \
-# -X PUT "https://api.piivault.com/api/profiles/GetPolyIdBulk" \
-# -H "Authorization: Bearer ${API_TOKEN}" \
-# -H "Content-Type: application/json" \
-# -H "Content-Type: application/json" -d @test-profiles.json
+echo
+echo "START //${HOSTNAME}/api/profiles/GetPolyIdBulk: $(date)"
+echo
 
-## -- PUT /api/profiles/ForgetProfile --
+curl -k \
+ -X PUT "https://${HOSTNAME}/api/profiles/GetPolyIdBulk" \
+ -H "Authorization: Bearer ${API_TOKEN}" \
+ -H "Content-Type: application/json" \
+ -H "Content-Type: application/json" -d @${REQUEST} | jq '.' > getpolyidbulk-reponse.json
 
-#curl -v \
-# -X PUT "https://api.piivault.com/api/profiles/ForgetProfile" \
-# -H "Authorization: Bearer ${API_TOKEN}" \
-# -H "Content-Type: application/json" \
-# -H "Content-Type: application/json" -d @test-forget-profile.json
+echo
+echo "FINIS GetPolyIdBulk: $(date)"
+echo
 
+;;
+
+ForgetProfile)
+#  -- PUT /api/profiles/ForgetProfile --
+
+echo
+echo "START /api/profiles/ForgetProfile: $(date)"
+echo
+
+curl -v \
+ -X PUT "https://${HOSTNAME}/api/profiles/ForgetProfile" \
+ -H "Authorization: Bearer ${API_TOKEN}" \
+ -H "Content-Type: application/json" \
+ -H "Content-Type: application/json" -d @${RESPONSE}
+
+echo
+echo "FINIS ForgetProfile: $(date)"
+echo
+
+;;
+
+PurgeAccountProfile)
 ## -- PUT PurgeAccountProfiles --
 
-#curl -v \
-#-X PUT "https://api.piivault.com/api/profiles/PurgeAccountProfiles" \
-#-H "Authorization: Bearer ${API_TOKEN}" \
-#-H "Content-Length: 0"
+echo
+echo "START /api/profiles/PurgeAccountProfiles: $(date)"
+echo
+
+curl -k \
+-X PUT "https://${HOSTNAME}/api/profiles/PurgeAccountProfiles" \
+-H "Authorization: Bearer ${API_TOKEN}" \
+-H "Content-Length: 0"
+
+echo
+echo "FINIS PurgeAccountProfiles: $(date)"
+echo
+
+;;
+
+Match)
+## -- Generate match table --
+echo
+echo "START GenerateMatchTable: $(date)"
+echo
+
+time curl -k \
+ -X POST "https://${HOSTNAME}/api/match" \
+ -H "Authorization: Bearer ${API_TOKEN}" \
+ -H "Content-Type: application/json" \
+ -H "Content-Type: application/json" -d @${REQUEST}
+
+echo
+echo "END   GenerateMatchTable: $(date)"
+echo
+
+
+## -- Get  match table --
+echo
+echo "START GetMatchList: $(date)"
+echo
+
+curl -k \
+ "https://${HOSTNAME}/api/match" \
+ -H "Authorization: Bearer ${API_TOKEN}" | jq '.' >  match-response.json
+
+echo
+echo "END   GetMatchList: $(date)"
+echo
+
+;;
+
+GetProfilePseudonym)
+
+echo
+echo "START //${HOSTNAME}/api/profiles/GetProfilePseudonym: $(date)"
+echo
+
+time curl -k \
+ -X PUT "https://${HOSTNAME}/api/profiles/GetProfilePseudonym" \
+ -H "Authorization: Bearer ${API_TOKEN}" \
+ -H "Content-Type: application/json" \
+ -H "Content-Type: application/json" -d @${REQUEST} | jq '.' > getprofilepseudonym-response.json
+
+echo
+echo "FINIS GetProfilePseudonym: $(date)"
+echo
+
+;;
+
+*)
+
+echo
+echo "Unknown command verb; check source"
+echo
+
+;;
+
+esac
 
